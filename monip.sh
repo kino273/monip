8fef898c09daa6cd2c152668fb6cc0e3d59a4628
kino273 <kino273@gmail.com>
2015-12-01 11:49:33 +0900

    init

---
+++ monip.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+x=$(readlink -f $0)
+dir=$(dirname $x)
+file=$dir/qqq
+tmp=$dir/ttt
+
+host -s myip.opendns.com resolver1.opendns.com > $file
+touch $tmp
+diff $file $tmp
+if [ $? = 0 ]
+then
+  echo "no update"
+  exit 0
+fi
+cp $file $tmp
+
+cd $dir
+
+ccencrypt -k key $file
+
+git add $file.cpt
+git commit -m update
+git push
+
